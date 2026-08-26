51befc614fbdb77873d8e2674367b760b04da43e
Kirk Ventura <kirkbrianventura@yahoo.com>
2026-08-25 20:48:56 -0700

    hello world

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream> //g++ main.cpp (g++ is the name of the compiler and main.cpp is the name of the file to be compiled)
+using namespace std; // to execute use ./a.out
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
