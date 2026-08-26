a6894518572c5ec04b2d549838d6072876b11bac
Kirk Ventura <kirkbrianventura@yahoo.com>
2026-08-25 21:17:19 -0700

    version 1 cs

--- main.cpp
+++ main.cpp
@@ -2,6 +2,18 @@
 using namespace std; // to execute use ./a.out
 
 int main() {
-    cout << "Hello, World!" << endl;
+    cout << "HH   HH  EEEEEEE  LL       LL        OOOOOO" << endl;
+    cout << "HH   HH  EE       LL       LL       OO    OO" << endl;
+    cout << "HHHHHHH  EEEEE    LL       LL       OO    OO" << endl;
+    cout << "HH   HH  EE       LL       LL       OO    OO" << endl;
+    cout << "HH   HH  EEEEEEE  LLLLLLL  LLLLLLL   OOOOOO" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "WW         WW   OOOOOO   RRRRRR   LL       DDDDDD" << endl;
+    cout << "WW         WW  OO    OO  RR   RR  LL       DD   DD" << endl;
+    cout << " WW  WWW  WW   OO    OO  RRRRRRR  LL       DD   DD" << endl;
+    cout << "  WWWW WWWW    OO    OO  RR  RR   LL       DD   DD" << endl;
+    cout << "   WW   WW      OOOOOO   RR   RR  LLLLLLL  DDDDDD" << endl;
+
     return 0;
 }
