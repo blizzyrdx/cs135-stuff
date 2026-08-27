4cc17553613c9d7e4e0ec634273e2eb1171c01a0
Kirk Ventura <kirkbrianventura@yahoo.com>
2026-08-27 12:54:03 -0700

    week 1

--- week 1/helloworld.cpp
+++ week 1/helloworld.cpp
@@ -1,17 +1,22 @@
-#include <iostream> //g++ main.cpp (g++ is the name of the compiler and main.cpp is the name of the file to be compiled)
-using namespace std; // to execute use ./a.out
+#include <iostream>
+using namespace std; 
 
-int main() {
+int main() 
+{
+    // top of the output
+    cout << "CS135 Assignment 0" << endl;
+    // hello part
+    cout << "" << endl;
     cout << "HH   HH  EEEEEEE  LL       LL        OOOOOO" << endl;
     cout << "HH   HH  EE       LL       LL       OO    OO" << endl;
     cout << "HHHHHHH  EEEEE    LL       LL       OO    OO" << endl;
     cout << "HH   HH  EE       LL       LL       OO    OO" << endl;
     cout << "HH   HH  EEEEEEE  LLLLLLL  LLLLLLL   OOOOOO" << endl;
     cout << "" << endl;
-    cout << "" << endl;
+    // world part
     cout << "WW         WW   OOOOOO   RRRRRR   LL       DDDDDD" << endl;
     cout << "WW         WW  OO    OO  RR   RR  LL       DD   DD" << endl;
-    cout << " WW  WWW  WW   OO    OO  RRRRRRR  LL       DD   DD" << endl;
+    cout << " WW  WWW  WW   OO    OO  RRRRRR   LL       DD   DD" << endl;
     cout << "  WWWW WWWW    OO    OO  RR  RR   LL       DD   DD" << endl;
     cout << "   WW   WW      OOOOOO   RR   RR  LLLLLLL  DDDDDD" << endl;
 
---
+++ week 1/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello CodeGrade!\n";
+	return 0;
+} 
