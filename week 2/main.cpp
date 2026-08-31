2a8fa6e3c4d2554fd7cef9931a75ebea946c0eae
Kirk Ventura <kirkbrianventura@yahoo.com>
2026-08-31 08:50:01 -0700

    update week 2 main

--- week 2/main.cpp
+++ week 2/main.cpp
@@ -7,9 +7,12 @@ using namespace std;
 
 // Identifiers must only contain letters, digits, and underscore (like monsterHealth); cannot have namespaces
 // Reserved Words cannot be identifier
+// Puts this datatype into RAM
 
 int main () {
     int monsterHealth = 100; //stores it in RAM
     cout << "Monster's health: " << monsterHealth << "\n";
     return 0;
 }
+
+//data types: integer (whole numbers)
