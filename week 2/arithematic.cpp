6ef5af9d1afabfc5fdcacbc0458eb03197e51d8a
Kirk Ventura <kirkbrianventura@yahoo.com>
2026-08-31 09:39:41 -0700

    added initial health

--- week 2/arithematic.cpp
+++ week 2/arithematic.cpp
@@ -6,10 +6,11 @@ int main () {
     
     int monsterHealth = 100; //you can put a comma in between if you assign values to said integers
     int damage_taken;
-    cout << "How much damage did the monster take?";
+    cout << "Current Monster Health: " << monsterHealth << endl;
+    cout << "How much damage did the monster take? " << endl;
     cin >> damage_taken;
 
     int new_health = monsterHealth - damage_taken;
-    cout << "New Health:" << new_health << endl;
+    cout << "New Health: " << new_health << endl;
     return 0;
 } 
