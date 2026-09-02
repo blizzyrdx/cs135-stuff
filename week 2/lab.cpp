86fa30a2e1b1a88bc857975cd097d0e6664c55a5
Kirk Ventura <kirkbrianventura@yahoo.com>
2026-09-01 21:31:48 -0700

    lab 2

---
+++ week 2/lab.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+
+/* Goal Of Lab
+Enter the first number (floating-point): 12.7
+Enter the second number (floating-point): 3.4
+
+Floating-point results:
+Sum: 16.1
+Difference: 9.3
+Product: 43.18
+Quotient: 3.73529
+
+Integer results (using truncated values):
+Integer Division Quotient: 4
+Modulus: 0 */
