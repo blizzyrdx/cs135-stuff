285d7bbb6d40150ad8bbcd68242993c58c2284b2
Kirk Ventura <kirkbrianventura@yahoo.com>
2026-09-01 21:34:04 -0700

    added goals

---
+++ week 2/assignment1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    return 0;
+}
+
+/* Goal Of Assignment
+alex-imac24@alex as1 % g++ main.cpp
+alex-imac24@alex as1 % ./a.out
+How many door bundles are needed? 3
+How much does each bundle cost? $1000.00
+
+Bundle Quantity: 3
+Bundle Cost: $1000
+Total Doors Purchased: 300
+Single Door Cost: $10
+Total Cost: $3000
+Single Door Sale Price: $17.5
+Total Sale Price: $5250
+Single Door Profit: $7.5
+Total Profit: $2250
+*/
