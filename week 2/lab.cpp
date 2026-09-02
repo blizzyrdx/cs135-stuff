1ccaa6d277a42613b0eb50c15ae30b4cc8ce447d
Kirk Ventura <kirkbrianventura@yahoo.com>
2026-09-02 08:40:45 -0700

    comment

--- week 2/lab.cpp
+++ week 2/lab.cpp
@@ -3,17 +3,26 @@
 using namespace std;
 
 int main () {
+
+    // define two floating-point numbers from user input
+
     double num1, num2;
     cout << "Enter the first number (floating-point): ";
     cin >> num1;
     cout << "Enter the second number (floating-point): ";
     cin >> num2;
     cout << "Floating-point results:" << endl;
+
+    // Operations
+
     cout << "Sum: " << num1 + num2 << endl;
     cout << "Difference: " << num1 - num2 << endl;
     cout << "Product: " << num1 * num2 << endl;
     cout << "Quotient: " << num1 / num2 << endl;
     cout << "" << endl;
+
+    //not done yet
+
     cout << "Integer results (using truncated values):" << endl; 
     cout << "Integer Division Quotient: " << endl;
     return 0;
