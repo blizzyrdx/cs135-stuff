073c4f6bbc464fc75e734fd848d7c658867b2f88
Kirk Ventura <kirkbrianventura@yahoo.com>
2026-09-09 08:38:50 -0700

    iomanip

--- week 3/main.cpp
+++ week 3/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main () { 
+
+    //defines the amount of scimimtars and whips looted by the player
+    cout << "How many scimitars did you loot? ";
+    int scimitars;
+    cout << "How many whips did you loot? ";
+    int whips; 
+
+    // decimals
+
+    const double scimitar_value = 59750.55; 
+    const double whip_value = 1487961.22;
 
     return 0;
 }
