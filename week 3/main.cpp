4fcecc62cb9d4bdee8416c73a91d71331b968810
Kirk Ventura <kirkbrianventura@yahoo.com>
2026-09-09 08:36:17 -0700

    parameters

--- week 3/main.cpp
+++ week 3/main.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 
 int main () { 
-    
+
     return 0;
 }
+
+// reads how many scimitars and whips a player looted and then a loot table
+// that displays how much everyting is worth 
+// Item, Qty, Each, Total
