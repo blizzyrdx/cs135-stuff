46dfe6e7a036b5990d575e7860f8f2e3b1faad4b
Kirk Ventura <kirkbrianventura@yahoo.com>
2026-09-09 09:09:33 -0700

    tables

--- week 3/class.cpp
+++ week 3/class.cpp
@@ -23,12 +23,18 @@ int main () {
     //total 
     double total_Scimitar = qty_Scimitar * each1;
     double total_Whip = qty_Whip * each2;
-    
-    //table
-    cout << fixed << setprecision(2) << setw(10) << left << weapon1 << right << setprecision(0) << setw(5) << qty_Scimitar << setw(15) << each1 << setw(15) << total_Scimitar << endl;
-    cout << fixed << setprecision(2) << setw(10) << left << weapon2 << right << setprecision(0) << setw(5) << qty_Whip << setw(15) << each2 << setw(15) << total_Whip << endl;
 
+    //table
+    cout << setw(15) << left << "Item" << right << setw(10) << "Qty" << setw(15) << "Each" << setw(15) << "Total" << endl;
+    cout << setw(15) << left << weapon1 << right << setw(10) << qty_Scimitar << setw(15) << each1 << setw(15) << total_Scimitar << endl;
+    cout << setw(15) << left << weapon2 << right << setw(10) << qty_Whip << setw(15) << each2 << setw(15) << total_Whip << endl;
+    
     return 0;
 }
 
 //setw(int) where int is the number of spaces at minimum to provide for the next piece of data output
+
+/* 
+cout << fixed << setprecision(2) << setw(10) << left << weapon1 << right << setprecision(0) << setw(5) << qty_Scimitar << setw(15) << each1 << setw(15) << total_Scimitar << endl;
+cout << fixed << setprecision(2) << setw(10) << left << weapon2 << right << setprecision(0) << setw(5) << qty_Whip << setw(15) << each2 << setw(15) << total_Whip << endl;
+*/
