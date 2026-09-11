7004ca52127fc7848f639e0568fc0517e9703e17
Kirk Ventura <kirkbrianventura@yahoo.com>
2026-09-11 10:51:59 -0700

    fixed bug

--- week 3/main.cpp
+++ week 3/main.cpp
@@ -20,10 +20,19 @@ int main () {
     cin >> magic_level;
     cout << "Enter your Magic bonus: ";
     cin >> magic_bonus;
+    cout << "\n";
 
     //Magic Hit Formula
-    int max_hit = floorf(Base_Damage * (1 + magic_level/100) * sqrt(1 + magic_bonus/100));
+    int max_hitBolt = floorf(Fire_Bolt * (1 + magic_level/100) * sqrt(1 + magic_bonus/100));
+    int max_hitBlast = floorf(Fire_Blast * (1 + magic_level/100) * sqrt(1 + magic_bonus/100));
+    int max_hitWave = floorf(Fire_Wave * (1 + magic_level/100) * sqrt(1 + magic_bonus/100));
 
     //Table Output
+    cout << setw(13) << left << "Spell" << setw(11) << "BaseDmg" << setw(8) << "Level" << setw(5) << right << "Bonus" << setw(9) << "MaxHit\n";
+    cout << setw(46) << setfill('-') << "\n";
+    cout << setfill(' ') << setw(13) << left << "Fire Bolt" << setw(11) << Fire_Bolt << setw(8) << magic_level << setw(5) << right << magic_bonus << setw(9) << max_hitBolt << endl;
+    cout << setfill(' ') << setw(13) << left << "Fire Blast" << setw(11) << Fire_Blast << setw(8) << magic_level << setw(5) << right << magic_bonus << setw(9) << max_hitBlast << endl;
+    cout << setfill(' ') << setw(13) << left << "Fire Wave" << setw(11) << Fire_Wave << setw(8) << magic_level << setw(5) << right << magic_bonus << setw(9) << max_hitWave << endl;
+
     return 0;
 }
