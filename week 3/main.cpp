394b2e1d0be10e5dfcf8e7dee6c859b0bef3bb0e
Kirk Ventura <kirkbrianventura@yahoo.com>
2026-09-11 06:35:11 -0700

    structure

--- week 3/main.cpp
+++ week 3/main.cpp
@@ -10,6 +10,12 @@ int main () {
     const int Fire_Bolt = 12;
     const int Fire_Blast = 16;
     const int Fire_Wave = 20;
-    
+
+    //Prompt user to enter magic level and magic bonus
+    //integer values for input
+
+    //Magic Hit Formula
+
+    //Table Output
     return 0;
 }
