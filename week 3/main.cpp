a474ccb3011f0c90e914546b525aa0b8cf27de8b
Kirk Ventura <kirkbrianventura@yahoo.com>
2026-09-14 08:06:05 -0700

    new

--- week 3/main.cpp
+++ week 3/main.cpp
@@ -27,8 +27,8 @@ int main () {
     int max_hitWave = floor(Fire_Wave * (1 + (magic_level/100.0)) * sqrt(1 + (magic_bonus/100.0)));
 
     //Table Output
-    cout << setw(13) << left << "Spell" << setw(11) << "BaseDmg" << setw(8) << "Level" << setw(5) << right << "Bonus" << setw(9) << "MaxHit\n";
-    cout << setw(46) << setfill('-') << "\n";
+    cout << setw(13) << left << "Spell" << setw(11) << "BaseDmg" << setw(8) << "Level" << setw(5) << right << "Bonus" << setw(10) << "MaxHit\n";
+    cout << setw(47) << setfill('-') << "\n";
     cout << setfill(' ') << setw(13) << left << "Fire Bolt" << setw(11) << Fire_Bolt << setw(8) << magic_level << setw(5) << right << magic_bonus << setw(9) << max_hitBolt << endl;
     cout << setfill(' ') << setw(13) << left << "Fire Blast" << setw(11) << Fire_Blast << setw(8) << magic_level << setw(5) << right << magic_bonus << setw(9) << max_hitBlast << endl;
     cout << setfill(' ') << setw(13) << left << "Fire Wave" << setw(11) << Fire_Wave << setw(8) << magic_level << setw(5) << right << magic_bonus << setw(9) << max_hitWave << endl;
