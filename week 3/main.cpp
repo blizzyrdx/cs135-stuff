36d9cbf7ea964aab5dd1c7c48867f077b32a1f13
Kirk Ventura <kirkbrianventura@yahoo.com>
2026-09-14 18:23:05 -0700

    new

--- week 3/main.cpp
+++ week 3/main.cpp
@@ -1,37 +1,17 @@
 #include <iostream>
-#include <iomanip> //for table
-#include <cmath> //for math
+#include <iomanip>
+#include <cmath>
 
 using namespace std;
 
 int main () {
 
-    //Constant Spells
-    const int Fire_Bolt = 12;
-    const int Fire_Blast = 16;
-    const int Fire_Wave = 20;
-    int magic_level = 0;
-    int magic_bonus = 0;
+    // initialize the stuff
 
-    //Prompt user to enter magic level and magic bonus
-    //integer values for input
-    cout << "Enter your Magic level: ";
-    cin >> magic_level;
-    cout << "Enter your Magic bonus: ";
-    cin >> magic_bonus;
-    cout << "\n";
+    // Formulas
 
-    //Magic Hit Formula
-    int max_hitBolt = floor(Fire_Bolt * (1 + (magic_level/100.0)) * sqrt(1 + (magic_bonus/100.0)));
-    int max_hitBlast = floor(Fire_Blast * (1 + (magic_level/100.0)) * sqrt(1 + (magic_bonus/100.0)));
-    int max_hitWave = floor(Fire_Wave * (1 + (magic_level/100.0)) * sqrt(1 + (magic_bonus/100.0)));
-
-    //Table Output
-    cout << setw(13) << left << "Spell" << setw(11) << "BaseDmg" << setw(8) << "Level" << setw(5) << right << "Bonus" << setw(10) << "MaxHit\n";
-    cout << setw(47) << setfill('-') << "\n";
-    cout << setfill(' ') << setw(13) << left << "Fire Bolt" << setw(11) << Fire_Bolt << setw(8) << magic_level << setw(5) << right << magic_bonus << setw(9) << max_hitBolt << endl;
-    cout << setfill(' ') << setw(13) << left << "Fire Blast" << setw(11) << Fire_Blast << setw(8) << magic_level << setw(5) << right << magic_bonus << setw(9) << max_hitBlast << endl;
-    cout << setfill(' ') << setw(13) << left << "Fire Wave" << setw(11) << Fire_Wave << setw(8) << magic_level << setw(5) << right << magic_bonus << setw(9) << max_hitWave << endl;
+    // Output
+    
 
     return 0;
 }
