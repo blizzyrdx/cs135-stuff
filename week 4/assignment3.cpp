799ea670824b609bdfa0af8316c07ac65583e6ad
Kirk Ventura <kirkbrianventura@yahoo.com>
2026-09-15 08:11:27 -0700

    structure complete

--- week 4/assignment3.cpp
+++ week 4/assignment3.cpp
@@ -5,6 +5,14 @@ using namespace std;
 
 int main () {
 
-    //
+    // first read user's level as an INTEGER
+
+    // then validate if its okay
+
+    // Read the user's slayer level as an integer
+
+    // 
+
+
     return 0;
 }
