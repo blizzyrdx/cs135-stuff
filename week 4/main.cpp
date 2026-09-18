523aa51399d3564aa8c401bdc136f482adb4ec3c
Kirk Ventura <kirkbrianventura@yahoo.com>
2026-09-18 16:19:43 -0700

    slayer

--- week 4/main.cpp
+++ week 4/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cmath>
+#include <string>
 
 using namespace std;
 
@@ -13,24 +13,63 @@ int main() {
 
     //define style_int
     int style_int = 0;
+    string melee = "";
+    string spell = "";
 
     //ask for the combat style as an integer
     cout << "Choose a style (1=Melee -or- 2=Magic): ";
     cin >> style_int;
 
     // Melee
-    if (style_int = 1) {
+    if (style_int == 1) {
     cout << "Choose a weapon (dagger or scimitar): ";
+    cin >> melee;
+
+        if (melee == "dagger") {
+        cout << "The dagger weapon causes" << 10 * melee_dagger << "damage with bonus" << melee_dagger << endl;
+        return 0; 
+        }
+        if (melee == "scimitar") {
+        cout << "The scimitar weapon causes" << 10 * melee_scimitar << " damage with bonus" << melee_scimitar << endl;
+        return 0; 
+        }
+
+        else {
+        cout << "Error: Invalid Weapon\n";
+        return 0;
+        }
+
+    return 0;
     }
 
     // Magic
-    if (style_int = 2) {
-    cout << "Choose a weapon (dagger or scimitar): ";
+    if (style_int == 2) {
+    cout << "Choose a spell (bolt or blast): ";
+    cin >> spell;
+
+        if (melee == "bolt") {
+        cout << "The bolt spell causes" << 10 * magic_bolt << "damage with bonus" << magic_bolt << endl;
+        return 0; 
+        }
+        if (melee == "blast") {
+        cout << "The blast spell causes" << 10 * magic_blast << " damage with bonus" << magic_blast << endl;
+        return 0; 
+        }
+
+        else {
+        cout << "Error: Invalid Spell.\n";
+        return 0;
+        }
+
+        
+    return 0;
     }
 
     // Invalid
-    if (style_int =! 1 || 2)
+    else {
     cout << "Error; Invalid Number.";
+    return 0;
+    }
 
     return 0;
 }
