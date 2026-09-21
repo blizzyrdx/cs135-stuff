934e577321e3c7155e3804df1651a3f6b2a7d7ba
Kirk Ventura <kirkbrianventura@yahoo.com>
2026-09-21 08:37:25 -0700

    new week

--- week 4/main.cpp
+++ week 4/main.cpp
@@ -13,6 +13,8 @@ int main() {
 
     //define style_int
     int style_int = 0;
+
+    //define melee and spell
     string melee = "";
     string spell = "";
 
@@ -22,20 +24,24 @@ int main() {
 
     // Melee
     if (style_int == 1) {
+    cout << endl;
     cout << "Choose a weapon (dagger or scimitar): ";
     cin >> melee;
 
         if (melee == "dagger") {
-        cout << "The dagger weapon causes" << 10 * melee_dagger << "damage with bonus" << melee_dagger << endl;
+        cout << endl;
+        cout << "The dagger weapon  causes " << 10 * melee_dagger << " damage with bonus " << melee_dagger << "." << endl;
         return 0; 
         }
         if (melee == "scimitar") {
-        cout << "The scimitar weapon causes" << 10 * melee_scimitar << " damage with bonus" << melee_scimitar << endl;
+        cout << endl;
+        cout << "The scimitar weapon  causes " << 10 * melee_scimitar << " damage with bonus " << melee_scimitar << "." << endl;
         return 0; 
         }
 
+        //invalid weapon
         else {
-        cout << "Error: Invalid Weapon\n";
+        cout << "Error: Invalid weapon\n";
         return 0;
         }
 
@@ -44,20 +50,23 @@ int main() {
 
     // Magic
     if (style_int == 2) {
+    cout << endl;
     cout << "Choose a spell (bolt or blast): ";
     cin >> spell;
 
-        if (melee == "bolt") {
-        cout << "The bolt spell causes" << 10 * magic_bolt << "damage with bonus" << magic_bolt << endl;
+        if (spell == "bolt") {
+        cout << endl;
+        cout << "The bolt spell  causes " << 10 * magic_bolt << " damage with bonus " << magic_bolt << "." << endl;
         return 0; 
         }
-        if (melee == "blast") {
-        cout << "The blast spell causes" << 10 * magic_blast << " damage with bonus" << magic_blast << endl;
+        if (spell == "blast") {
+        cout << endl;
+        cout << "The blast spell  causes " << 10 * magic_blast << " damage with bonus " << magic_blast << "." << endl;
         return 0; 
         }
 
         else {
-        cout << "Error: Invalid Spell.\n";
+        cout << "Error: Invalid spell\n";
         return 0;
         }
 
@@ -66,8 +75,9 @@ int main() {
     }
 
     // Invalid
+    // superflous 
     else {
-    cout << "Error; Invalid Number.";
+    cout << "Error: Invalid number";
     return 0;
     }
 
