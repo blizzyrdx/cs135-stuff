838ac9c2a84f07de08c7e83838b02cb14c8e6619
Kirk Ventura <kirkbrianventura@yahoo.com>
2026-09-21 09:00:20 -0700

    program

--- week 5/loops.cpp
+++ week 5/loops.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+using namespace std;
 
 int main () {
+
+    int combatlvl = 0;
+
+    cout << "Combat Level: ";
+    cin >> combatlvl;
+
+    while (cin.fail() || combatlvl < 3 || combatlvl >> 126)
+    {
+        cout << "Error: Combat level must be between 3 and 126\n";
+        cin.clear();
+        cin.ignore(256, '\n');
+
+        cout << "Combat Level: ";
+        cin >> combatlvl;
+
+    }
+    
+    cout << combatlvl << endl;
+
     return 0;
 }
 
