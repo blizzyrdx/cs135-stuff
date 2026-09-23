9d398efe45635595ff87594ea2f164c10c017336
Kirk Ventura <kirkbrianventura@yahoo.com>
2026-09-23 09:19:52 -0700

    syntax

--- week 5/newlibrary.cpp
+++ week 5/newlibrary.cpp
@@ -6,3 +6,15 @@ using namespace std;
 int main () {
     return 0;
 }
+
+/* New Syntax
+ifstream, ofstream
+
+step 1: include fstream
+step 2: ifsteram or ofstream 
+step 3: Open the file fstream.open(path)
+step 4: read of write  
+if stream >> variable
+ofstream << data
+step 5: close the file - fstream.close()
+*/
