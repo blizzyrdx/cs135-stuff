c0c7b76fb1c89222aaf80b33bd93329c50393563
Kirk Ventura <kirkbrianventura@yahoo.com>
2026-09-25 13:25:59 -0700

    3

--- week 6/main.cpp
+++ week 6/main.cpp
@@ -3,5 +3,37 @@
 using namespace std;
 
 int main() {
+
+    //loop!
+
+    // remember to initialize variable
+    // attack variable
+    int attack = 0;
+
+    // define variables
+    // initialize
+    int monster_health = 0;
+    int player_health = 0;
+
+    // Output the monster and player's health
+    cout << "Player Health: " << player_health << endl;
+    cout << "Monster Health: " << monster_health << endl;
+    cout << endl;
+    cout << "1-Stab -or- 2-Slash: "; 
+    cin >> attack;
+
+    // if fail
+    if (cin.fail() || attack < 1 || attack > 2) {
+     cout << "Error: Invalid selection";
+    }
+
+    else if (attack = 1) {
+        cout << "turtle";
+    }
+
+    else if (attack = 2) {
+        cout << "monkey";
+    }
+
     return 0;
 }
